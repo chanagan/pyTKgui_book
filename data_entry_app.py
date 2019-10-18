f6cd1e10681cf2a93a924874e03e03e5c8817a44
chris.hanagan <chris.hanagan@gmail.com>
2019-10-18 17:02:48 -0400

    Add some additional code to earlier modules

--- data_entry_app.py
+++ data_entry_app.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from decimal import Decimal, InvalidOperation
+
 import os
 import csv
 
@@ -191,16 +193,16 @@ class DataRecordForm(tk.Frame):
 		)
 		self.inputs['Notes'].grid(sticky="w", row=3, column=0)
 
-		def get(self):
-			data = {}
-			for key, widget in self.inputs.items():
-				data[key] = widget.get()
-			return data
+	def get(self):
+		data = {}
+		for key, widget in self.inputs.items():
+			data[key] = widget.get()
+		return data
 
-		def reset(self):
-			for widget in self.inputs.values():
-				widget.set('')
-			self.reset()
+	def reset(self):
+		for widget in self.inputs.values():
+			widget.set('')
+		self.reset()
 
 
 class ValidatedMixin:
@@ -334,6 +336,110 @@ class DateEntry(ValidatedMixin, ttk.Entry):
 		return valid
 
 
+class ValidatedSpinbox(ValidatedMixin, tk.Spinbox):
+
+	def __init__(self, *args, min_var=None, max_var=None,
+			focus_update_var=None, from_='-Infinity',
+			to='Infinity', **kwargs):
+		super().__init__(*args, from_=from_, to=to, **kwargs)
+		self.resolution = Decimal(str(kwargs.get('increment',
+			'1.0')))
+		self.precision = (
+			self.resolution
+				.normalize()
+				.as_tuple()
+				.exponent)
+
+		# there should always be a variable,
+		# or some of our code will fail
+		self.variable = kwargs.get('textvariable') or tk.DoubleVar()
+
+		if min_var:
+			self.min_var = min_var
+			self.min_var.trace('w', self._set_minimum)
+		if max_var:
+			self.max_var = max_var
+			self.max_var.trace('w', self._set_maximum)
+		self.focus_update_var = focus_update_var
+		self.bind('<FocusOut>', self._set_focus_update_var)
+
+	def _set_focus_update_var(self, event):
+		value = self.get()
+		if self.focus_update_var and not self.error.get():
+			self.focus_update_var.set(value)
+
+	def _set_minimum(self, *args):
+		current = self.get()
+		try:
+			new_min = self.min_var.get()
+			self.config(from_=new_min)
+		except (tk.TclError, ValueError):
+			pass
+		if not current:
+			self.delete(0, tk.END)
+		else:
+			self.variable.set(current)
+		self.trigger_focusout_validation()
+
+	def _key_validate(self, char, index, current,
+			proposed, action, **kwargs):
+		valid = True
+		min_val = self.cget('from')
+		max_val = self.cget('to')
+		no_negative = min_val >= 0
+		no_decimal = self.precision >= 0
+		if action == '0':
+			return True
+
+		# First, filter out obviously invalid keystrokes
+		if any([
+			(char not in ('-1234567890.')),
+			(char == '-' and (no_negative or index != '0')),
+			(char == '.' and (no_decimal or '.' in current))
+		]):
+			return False
+
+		# At this point, proposed is either '-', '.', '-.',
+		# or a valid Decimal string
+		if proposed in '-.':
+			return True
+
+		# Proposed is a valid Decimal string
+		# convert to Decimal and check more:
+		proposed = Decimal(proposed)
+		proposed_precision = proposed.as_tuple().exponent
+
+		if any([
+			(proposed > max_val),
+			(proposed_precision < self.precision)
+		]):
+			return False
+
+		return valid
+
+	def _focusout_validate(self, **kwargs):
+		valid = True
+		value = self.get()
+		min_val = self.cget('from')
+
+		try:
+			value = Decimal(value)
+		except InvalidOperation:
+			self.error.set('Invalid number string: {}'.format(value))
+			return False
+
+		if value < min_val:
+			self.error.set('Value is too low (min {})'.format(min_val))
+			valid = False
+
+		max_val = self.cget('to')
+		if value > max_val:
+			self.error.set('Value is too high (max {})'.format(max_val))
+			valid = False
+
+		return valid
+
+
 class ValidatedCombobox(ValidatedMixin, ttk.Combobox):
 
 	def _key_validate(self, proposed, action, **kwargs):
@@ -373,7 +479,7 @@ class Application(tk.Tk):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-		self.title("ABQ Data Entry Application")
+		self.title("ABQ Data Entry Application - Single")
 		self.resizable(width=False, height=False)
 
 		ttk.Label(
