8e470198842797d9ee617d2c306c111c24a7af3a
Chris Hanagan <chris.hanagan@gmail.com>
2019-10-25 06:09:07 -0400

    Sync with Chapter 5

--- ABQ_Data_Entry/abq_data_entry/constants.py
+++ ABQ_Data_Entry/abq_data_entry/constants.py
@@ -1,6 +1,6 @@
 class FieldTypes:
 	string = 1
-	string_list =2
+	string_list = 2
 	iso_date_string = 3
 	long_string = 4
 	decimal = 5
--- ABQ_Data_Entry/abq_data_entry/models.py
+++ ABQ_Data_Entry/abq_data_entry/models.py
@@ -5,36 +5,37 @@ from .constants import FieldTypes as FT
 
 class CSVModel:
 	"""CSV file storage"""
+
 	fields = {
-		"Date"           : {'req': True, 'type': FT.iso_date_string},
-		"Time"           : {'req': True, 'type': FT.string_list,
-			'values'             : ['8:00', '12:00', '16:00', '20:00']},
-		"Technician"     : {'req': True, 'type': FT.string},
-		"Lab"            : {'req': True, 'type': FT.string_list,
-			'values'             : ['A', 'B', 'C', 'D', 'E']},
-		"Plot"           : {'req': True, 'type': FT.string_list,
-			'values'             : [str(x) for x in range(1, 21)]},
-		"Seed sample"    : {'req': True, 'type': FT.string},
-		"Humidity"       : {'req': True, 'type': FT.decimal,
-			'min'                : 0.5, 'max': 52.0, 'inc': .01},
-		"Light"          : {'req': True, 'type': FT.decimal,
-			'min'                : 0, 'max': 100.0, 'inc': .01},
-		"Temperature"    : {'req': True, 'type': FT.decimal,
-			'min'                : 4, 'max': 40, 'inc': .01},
+		"Date": {'req': True, 'type': FT.iso_date_string},
+		"Time": {'req': True, 'type': FT.string_list,
+				 'values': ['8:00', '12:00', '16:00', '20:00']},
+		"Technician": {'req': True, 'type': FT.string},
+		"Lab": {'req': True, 'type': FT.string_list,
+				'values': ['A', 'B', 'C', 'D', 'E']},
+		"Plot": {'req': True, 'type': FT.string_list,
+				 'values': [str(x) for x in range(1, 21)]},
+		"Seed sample": {'req': True, 'type': FT.string},
+		"Humidity": {'req': True, 'type': FT.decimal,
+					 'min': 0.5, 'max': 52.0, 'inc': .01},
+		"Light": {'req': True, 'type': FT.decimal,
+				  'min': 0, 'max': 100.0, 'inc': .01},
+		"Temperature": {'req': True, 'type': FT.decimal,
+						'min': 4, 'max': 40, 'inc': .01},
 		"Equipment Fault": {'req': False, 'type': FT.boolean},
-		"Plants"         : {'req': True, 'type': FT.integer,
-			'min'                : 0, 'max': 20},
-		"Blossoms"       : {'req': True, 'type': FT.integer,
-			'min'                : 0, 'max': 1000},
-		"Fruit"          : {'req': True, 'type': FT.integer,
-			'min'                : 0, 'max': 1000},
-		"Min Height"     : {'req': True, 'type': FT.decimal,
-			'min'                : 0, 'max': 1000, 'inc': .01},
-		"Max Height"     : {'req': True, 'type': FT.decimal,
-			'min'                : 0, 'max': 1000, 'inc': .01},
-		"Median Height"  : {'req': True, 'type': FT.decimal,
-			'min'                : 0, 'max': 1000, 'inc': .01},
-		"Notes"          : {'req': False, 'type': FT.long_string}
+		"Plants": {'req': True, 'type': FT.integer,
+				   'min': 0, 'max': 20},
+		"Blossoms": {'req': True, 'type': FT.integer,
+					 'min': 0, 'max': 1000},
+		"Fruit": {'req': True, 'type': FT.integer,
+				  'min': 0, 'max': 1000},
+		"Min Height": {'req': True, 'type': FT.decimal,
+					   'min': 0, 'max': 1000, 'inc': .01},
+		"Max Height": {'req': True, 'type': FT.decimal,
+					   'min': 0, 'max': 1000, 'inc': .01},
+		"Median Height": {'req': True, 'type': FT.decimal,
+						  'min': 0, 'max': 1000, 'inc': .01},
+		"Notes": {'req': False, 'type': FT.long_string}
 	}
 
 	def __init__(self, filename):
@@ -46,9 +47,7 @@ class CSVModel:
 		newfile = not os.path.exists(self.filename)
 
 		with open(self.filename, 'a') as fh:
-			csvwriter = csv.DictWriter(fh,
-				fieldnames=self.fields.keys())
+			csvwriter = csv.DictWriter(fh, fieldnames=self.fields.keys())
 			if newfile:
 				csvwriter.writeheader()
 			csvwriter.writerow(data)
-
